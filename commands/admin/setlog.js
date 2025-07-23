2dadece0a24c99b44a2bbd19a3f9df7618cbc1b2
Beluga2248 <113758661+Beluga2248@users.noreply.github.com>
2025-07-23 20:16:11 +0800

    setlog.js

--- commands/admin/setlog.js
+++ commands/admin/setlog.js
@@ -18,7 +18,7 @@ module.exports = {
     await settings.save();
 
     const embed = new EmbedBuilder()
-      .setTitle('✅ Log Channel Set')
+      .setTitle('<a:Yes:1011614293420150805:> Log Channel Set')
       .setDescription(`Logging will now go to ${channel}`)
       .setColor('Green');
 
