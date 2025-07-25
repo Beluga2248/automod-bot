232f6947868c90323c52298df54b38737efefee1
Beluga2248 <113758661+Beluga2248@users.noreply.github.com>
2025-07-26 00:09:09 +0800

    help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -36,7 +36,7 @@ module.exports = {
         const pages = categories.map((cat, idx) => {
             return new EmbedBuilder()
                 .setTitle("Help Menu")
-                .setDescription(`</info:1115209833218588722> **Use the buttons below to navigate!**\n\n__**${cat.toUpperCase()} COMMANDS -**__\n\n` +
+                .setDescription(`<a:info:1115209833218588722> **Use the buttons below to navigate!**\n\n__**${cat.toUpperCase()} COMMANDS -**__\n\n` +
                     commands[idx].map(cmd => `**/${cmd.name}**\n${cmd.description}`).join('\n\n')
                 )
                 .setColor("#2f3136")
@@ -62,7 +62,7 @@ module.exports = {
 
         collector.on('collect', async i => {
             if (i.user.id !== interaction.user.id) {
-                return i.reply({ content: "</Wrong:1017416697168269372:> You can't control this menu!", ephemeral: true });
+                return i.reply({ content: "<a:Wrong:1017416697168269372:> You can't control this menu!", ephemeral: true });
             }
 
             if (i.customId === 'first') page = 0;
