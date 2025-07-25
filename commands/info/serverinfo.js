17a33014d993d6ac199c667742af1857b3e182ea
Beluga2248 <113758661+Beluga2248@users.noreply.github.com>
2025-07-25 23:35:44 +0800

    serverinfo.js

--- commands/info/serverinfo.js
+++ commands/info/serverinfo.js
@@ -13,12 +13,12 @@ module.exports = {
       .setThumbnail(guild.iconURL({ dynamic: true }))
       .setColor('Green')
       .addFields(
-        { name: '<a:info:1115209833218588722:> Server ID', value: guild.id, inline: true },
-        { name: '<a:owner:1017416872515346462:> Owner', value: `<@${guild.ownerId}>`, inline: true },
-        { name: '<a:info:1115209833218588722:> Members', value: `${guild.memberCount}`, inline: true },
-        { name: '<a:info:1115209833218588722:> Roles', value: `${guild.roles.cache.size}`, inline: true },
-        { name: '<a:info:1115209833218588722:> Channels', value: `${guild.channels.cache.size}`, inline: true },
-        { name: '<a:info:1115209833218588722:> Created', value: `<t:${Math.floor(guild.createdTimestamp / 1000)}:F>`, inline: true }
+        { name: '</info:1115209833218588722> Server ID', value: guild.id, inline: true },
+        { name: '</owner:1017416872515346462> Owner', value: `<@${guild.ownerId}>`, inline: true },
+        { name: '</info:1115209833218588722> Members', value: `${guild.memberCount}`, inline: true },
+        { name: '</info:1115209833218588722> Roles', value: `${guild.roles.cache.size}`, inline: true },
+        { name: '</info:1115209833218588722> Channels', value: `${guild.channels.cache.size}`, inline: true },
+        { name: '</info:1115209833218588722> Created', value: `<t:${Math.floor(guild.createdTimestamp / 1000)}:F>`, inline: true }
       );
 
     await interaction.reply({ embeds: [embed] });
