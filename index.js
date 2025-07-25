b642be98c8df994a30d7d33ff029aa5f9c05077f
Beluga2248 <113758661+Beluga2248@users.noreply.github.com>
2025-07-25 22:39:59 +0800

    index.js

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ client.on('interactionCreate', async interaction => {
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+ // useNewUrlParser: true,
+  //useUnifiedTopology: true,
 }).then(() => {
   console.log('✅ Connected to MongoDB');
   client.login(process.env.DISCORD_TOKEN);
