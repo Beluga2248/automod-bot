f3c0b3f520a0d7a800bcc4ab84290db2ed0ea457
Beluga2248 <113758661+Beluga2248@users.noreply.github.com>
2025-07-25 23:31:26 +0800

    help.js

--- commands/general/help.js
+++ commands/general/help.js
@@ -36,11 +36,11 @@ module.exports = {
         const pages = categories.map((cat, idx) => {
             return new EmbedBuilder()
                 .setTitle("Help Menu")
-                .setDescription(`<a:info:1115209833218588722:> **Use the buttons below to navigate!**\n\n__**${cat.toUpperCase()} COMMANDS -**__\n\n` +
+                .setDescription(`</info:1115209833218588722> **Use the buttons below to navigate!**\n\n__**${cat.toUpperCase()} COMMANDS -**__\n\n` +
                     commands[idx].map(cmd => `**/${cmd.name}**\n${cmd.description}`).join('\n\n')
                 )
                 .setColor("#2f3136")
-                .setFooter({ text: `Page 1/${categories.length} • Support: https://cwkbot.fun/discord` });
+                .setFooter({ text: `Page 1/${categories.length} • Support: https://youtube.com/c/BeluTroll` });
         });
 
         let page = 0;
@@ -62,7 +62,7 @@ module.exports = {
 
         collector.on('collect', async i => {
             if (i.user.id !== interaction.user.id) {
-                return i.reply({ content: "<a:Wrong:1017416697168269372:> You can't control this menu!", ephemeral: true });
+                return i.reply({ content: "</Wrong:1017416697168269372:> You can't control this menu!", ephemeral: true });
             }
 
             if (i.customId === 'first') page = 0;
