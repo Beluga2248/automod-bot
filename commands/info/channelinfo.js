92e8348dd43f0e855a0dbb4ba62902c47f53dec2
Beluga2248 <113758661+Beluga2248@users.noreply.github.com>
2025-07-26 00:11:15 +0800

    channelinfo.js

--- commands/info/channelinfo.js
+++ commands/info/channelinfo.js
@@ -16,10 +16,10 @@ module.exports = {
       .setTitle(`📺 Channel Info: ${channel.name}`)
       .setColor('Blue')
       .addFields(
-        { name: '<a:info:1115209833218588722:> ID', value: channel.id, inline: true },
-        { name: '<a:info:1115209833218588722:> Type', value: ChannelType[channel.type], inline: true },
-        { name: '<a:info:1115209833218588722:> NSFW', value: channel.nsfw ? 'Yes' : 'No', inline: true },
-        { name: '<a:info:1115209833218588722:> Created', value: `<t:${Math.floor(channel.createdTimestamp / 1000)}:F>`, inline: true }
+        { name: '<a:info:1115209833218588722> ID', value: channel.id, inline: true },
+        { name: '<a:info:1115209833218588722> Type', value: ChannelType[channel.type], inline: true },
+        { name: '<a:info:1115209833218588722> NSFW', value: channel.nsfw ? 'Yes' : 'No', inline: true },
+        { name: '<a:info:1115209833218588722> Created', value: `<t:${Math.floor(channel.createdTimestamp / 1000)}:F>`, inline: true }
       );
 
     await interaction.reply({ embeds: [embed] });
