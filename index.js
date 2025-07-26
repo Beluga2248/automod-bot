c9939259074b9732ebbe73941d159e5e9cead91c
Beluga2248 <113758661+Beluga2248@users.noreply.github.com>
2025-07-26 09:14:58 +0800

    index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 require('dotenv').config();
 const { Client, GatewayIntentBits, Collection, Partials, EmbedBuilder } = require('discord.js');
 const fs = require('fs');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = (process.env.MONGO_URI);
 //const mongoose = require('mongoose');
 const path = require('path');
 const deploy = require ('./deploy-commands.js')
@@ -60,6 +62,29 @@ client.on('interactionCreate', async interaction => {
       }
     }
   }
+  
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
   // Handle other types of interactions here if needed
   // Example: button/menu interactions can go here
